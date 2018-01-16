1661dc08dd375118b7cad3fb75437d74e03992ad
passthesalt <austinlee@Austins-MacBook-Air.local>
2018-01-16 13:17:48 -0500

    updated hw1_1.c

---
+++ hw1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  if (argc == 1) {
+    printf("Error, too few arguments! \n");
+  }
+  else if (argc > 3) {
+    printf("Error, too many arguments! \n");
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      printf("arg[%i]=%s \n", i, argv[i]);
+    }
+  }
+
+  return 0;
+}
